62f7815e6f0fb9dada8f34f1dab0b017250b3487
maks00170 <maks.groshev4@mail.ru>
2024-07-09 12:06:20 +0300

    init_commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # BEAM
+Модель:
+https://drive.google.com/file/d/1LwDXGyJguMSeNrGfOYFsro4Lp1SVxWiL/view
+
+`Modules.py`:
+
+    1.STFT
+    2.MVDR
+    3.mask_estimator
+
+`pl_model.py`:
+
+    1. Spectral loss
+    2. pytorch_lightning model
+
+`data_valid.ipynb`: анализ предикта
+
+`train.ipynb`: пример (теста) запуска обучалки
+
+`RIR.ipynb`: пример генерации RIR
---
+++ pl_model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import pytorch_lightning as pl
+from torchmetrics import ScaleInvariantSignalNoiseRatio
+import torch.nn.functional as F
+import torch.nn as nn
+from Modules import STFT, MVDR, mask_estimator
+import torch
+
+class SpectralLoss(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, target_spec, predict_spec):
+        target = torch.log(target_spec.abs()+1e-5)
+        predict = torch.log(predict_spec.abs()+1e-5)
+        
+        return F.mse_loss(target, predict)
+    
+    
+class PL_model(pl.LightningModule):
+    def __init__(self, mic=1):
+        super().__init__()
+        self.mvdr = MVDR(0)
+        self.model_mask = mask_estimator(1)
+        fs = 16000  # Sampling frequency
+        window_length = int(0.025 * fs)  # 25 ms window length
+        window_shift = int(0.01 * fs)  # 10 ms window shift
+        self.stft = STFT(n_fft = window_length, hop=window_shift)
+        
+        self.criterion = SpectralLoss()
+        self.metric = ScaleInvariantSignalNoiseRatio()
+    def forward(self, x):
+        spec = self.stft.stft(x)
+        mask = self.model_mask(spec)
+        predict_spec = self.mvdr(spec, mask)
+        return predict_spec, self.stft.istft(predict_spec, x.shape[-1]) 
+    
+    def training_step(self, batch, batch_idx):
+        target_signal, mic_array_signal = batch
+        
+        predict_spec, predict_signal = self.forward(mic_array_signal)
+        loss = self.criterion(self.stft.stft(target_signal), predict_spec)
+        
+        metric = self.metric(target_signal, predict_signal.view(target_signal.size()))
+        self.log_dict(
+            {
+                "train_loss": loss,
+                "si_snr_train": metric,
+            },
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
+        )
+        
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        target_signal, mic_array_signal = batch
+        
+        predict_spec, predict_signal = self.forward(mic_array_signal)
+        loss = self.criterion(self.stft.stft(target_signal), predict_spec)
+        
+        metric = self.metric(target_signal, predict_signal.view(target_signal.size()))
+        
+        self.log_dict(
+            {
+                "valid_loss": loss,
+                "si_snr_valid": metric,
+            },
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
+        )
+        
+        
+    def configure_optimizers(self):
+        optimizer = torch.optim.RMSprop(self.parameters(), lr=1e-6)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer, T_0=40
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": scheduler,
+            "monitor": "valid_loss",
+        }
