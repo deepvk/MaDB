45f05dbe1a13d6f8cef6b9978dcf7ae082b0dd25
maks00170 <maks.groshev4@mail.ru>
2024-07-31 18:28:24 +0300

    add v5

---
+++ v5/Modules.py
@@ -0,0 +1,170 @@
+from typing import Tuple, Optional, Union
+import math
+import torch as th
+import torch
+import torch.nn as nn
+import torchaudio as ta
+from torchaudio.transforms import PSD
+import torch.nn.functional as F
+
+class STFT:
+    def __init__(self, n_fft: int = 4096, hop=1, pad: int = 0):
+        self.n_fft = n_fft
+        self.pad = pad
+        self.hop_length = hop
+
+    def __pad1d(
+        self,
+        x: torch.Tensor,
+        paddings: Tuple[int, int],
+        mode: str = "constant",
+        value: float = 0.0,
+    ):
+        """
+        Tiny wrapper around F.pad, designed to allow reflect padding on small inputs.
+        If the input is too small for reflect padding, we first add extra zero padding to the right before reflection occurs.
+        """
+        x0 = x
+        length = x.shape[-1]
+        padding_left, padding_right = paddings
+        if mode == "reflect":
+            max_pad = max(padding_left, padding_right)
+            if length <= max_pad:
+                extra_pad = max_pad - length + 1
+                extra_pad_right = min(padding_right, extra_pad)
+                extra_pad_left = extra_pad - extra_pad_right
+                paddings = (
+                    padding_left - extra_pad_left,
+                    padding_right - extra_pad_right,
+                )
+                x = F.pad(x, (extra_pad_left, extra_pad_right))
+        out = F.pad(x, paddings, mode, value)
+        assert out.shape[-1] == length + padding_left + padding_right
+        assert (out[..., padding_left : padding_left + length] == x0).all()
+        return out
+
+    def _spec(self, x: torch.Tensor):
+        *other, length = x.shape
+        x = x.reshape(-1, length)
+        z = th.stft(
+            x,
+            self.n_fft * (1 + self.pad),
+            self.hop_length or self.n_fft // 4,
+            window=th.hann_window(self.n_fft).to(x),
+            win_length=self.n_fft,
+            normalized=True,
+            center=True,
+            return_complex=True,
+            pad_mode="reflect",
+        )
+        _, freqs, frame = z.shape
+        return z.view(*other, freqs, frame)
+
+    def _ispec(self, z: torch.Tensor, length: int):
+        *other, freqs, frames = z.shape
+        n_fft = 2 * freqs - 2
+        z = z.view(-1, freqs, frames)
+        win_length = n_fft // (1 + self.pad)
+        is_mps = z.device.type == "mps"
+        if is_mps:
+            z = z.cpu()
+        z = th.istft(
+            z,
+            n_fft,
+            self.hop_length,
+            window=th.hann_window(win_length).to(z.real),
+            win_length=win_length,
+            normalized=True,
+            length=length,
+            center=True,
+        )
+        _, length = z.shape
+        return z.view(*other, length)
+
+    def stft(self, x: torch.Tensor):
+        hl = self.hop_length
+        x0 = x  # noqa
+        le = int(math.ceil(x.shape[-1] / self.hop_length))
+        pad = hl // 2 * 3
+        x = self.__pad1d(
+            x, (pad, pad + le * self.hop_length - x.shape[-1]), mode="reflect"
+        )
+        z = self._spec(x)[..., :-1, :]
+        z = z[..., 2 : 2 + le]
+        return z
+
+    def istft(self, z: torch.Tensor, length: int = 0, scale: Optional[int] = 0):
+        hl = self.hop_length // (4**scale)
+        z = F.pad(z, (0, 0, 0, 1))
+        z = F.pad(z, (2, 2))
+        pad = hl // 2 * 3
+        le = hl * int(math.ceil(length / hl)) + 2 * pad
+
+        x = self._ispec(z, length=le)
+
+        x = x[..., pad : pad + length]
+        return x
+    
+
+class MVDR(nn.Module):
+    
+    def __init__(self, ref_chennel):
+        super().__init__()
+        self.psd = PSD()
+        self.ref_channel = ref_chennel
+        
+        self.psd_speech_t = 0
+        self.psd_noise_t = 0
+        self.mask_sum_speech = 0
+        self.mask_sum_noise = 0
+        self.t = 0
+        
+    def reload(self):
+        self.psd_speech_t = 0
+        self.psd_noise_t = 0
+        self.mask_sum_speech = 0
+        self.mask_sum_noise = 0
+        self.t = 0
+        
+    def recurent_update(self, psd_n, psd_t, mask, mask_t): #snatch, still # psd_n - new
+        # psd_n - new
+        # psd_t - old, story, agr
+        # mask - new
+        # mast_t - old, story, agr
+        
+        numerator = mask_t / (mask_t + mask.sum(dim=-1))
+        
+        denominator = 1 / (mask_t + mask.sum(dim=-1))
+        psd_n = psd_t * numerator[..., None, None] + psd_n * denominator[..., None, None]
+        
+        return psd_n
+    
+    def forward(self, mix, mask_speech):
+        B, C, F, T = mix.shape
+        
+        mask_noise = 1 - mask_speech
+        
+        psd_speech = self.psd(mix, mask_speech)
+        psd_noise = self.psd(mix, mask_noise)
+        
+        ref_vector = torch.zeros((B, C), device=mix.device, dtype=torch.cdouble)  # (..., channel)
+        ref_vector[..., self.ref_channel].fill_(1)
+        
+        
+        if self.t == 0: # t_0
+            self.psd_speech_t = psd_speech
+            self.psd_noise_t = psd_noise
+            self.mask_sum_speech = mask_speech.sum(dim=-1)
+            self.mask_sum_noise = mask_noise.sum(dim=-1)
+            self.t = 1
+        else:
+            self.psd_speech_t = self.recurent_update(psd_speech, self.psd_speech_t, mask_speech, self.mask_sum_speech)
+            self.psd_speech_n = self.recurent_update(psd_noise, self.psd_noise_t, mask_noise, self.mask_sum_noise)
+            self.mask_sum_speech = self.mask_sum_speech + mask_speech.sum(dim=-1)
+            self.mask_sum_noise = self.mask_sum_noise + mask_noise.sum(dim=-1)
+            self.t += 1
+            
+        w_vetor = ta.functional.mvdr_weights_souden(self.psd_speech_t, self.psd_noise_t, ref_vector)
+        spec_enhanced = ta.functional.apply_beamforming(w_vetor, mix)
+
+        return spec_enhanced
---
+++ v5/model.py
@@ -0,0 +1,224 @@
+import torch
+import torch.nn as nn
+import math
+from torch.nn import functional as F
+
+class CausalConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1):
+        super(CausalConv2d, self).__init__()
+        self.padding_time = (kernel_size[1] - 1) * dilation
+        self.conv = nn.Conv2d(in_channels, 
+                              out_channels, 
+                              kernel_size, 
+                              stride, 
+                              padding=(0, self.padding_time), 
+                              dilation=dilation)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.padding_time != 0:
+            x = x[:, :, :, :-self.padding_time]
+        return x
+    
+
+class CausalConvTranspose2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1):
+        super(CausalConvTranspose2d, self).__init__()
+        self.padding_time = (kernel_size[1] - 1) * dilation
+        self.conv_transpose = nn.ConvTranspose2d(
+            in_channels, 
+            out_channels, 
+            kernel_size, 
+            stride, 
+            padding=0,
+            dilation=dilation
+        )
+    def forward(self, x):
+        x = self.conv_transpose(x)
+        if self.padding_time != 0:
+            x = x[:, :, :, :-self.padding_time]
+        return x
+
+class DownSample(nn.Module):
+    def __init__(self, 
+                 input_channel,
+                 out_channel,
+                 kernel_size,
+                 stride,
+                 activation,
+                 normalization
+                ):
+        super().__init__()
+        self.conv = nn.Sequential(
+            CausalConv2d(input_channel, out_channel, 
+                      kernel_size=kernel_size, stride=stride
+                     ), 
+            normalization, 
+            activation
+        )
+    def forward(self, x):
+        return self.conv(x)
+    
+class UpSample(nn.Module):
+    def __init__(self, 
+                 input_channel,
+                 out_channel,
+                 kernel_size,
+                 stride,
+                 activation,
+                 normalization
+                ):
+        super().__init__()
+        self.conv = nn.Sequential(
+            CausalConvTranspose2d(input_channel, out_channel, 
+                      kernel_size=kernel_size, stride=stride
+                     ), 
+            normalization, 
+            activation
+        )
+    def forward(self, x1, x2):
+        
+        diffY = x2.size()[2] - x1.size()[2]
+        x1 = F.pad(x1, [0, 0, diffY // 2, diffY - diffY // 2] )
+        x = torch.cat((x1,x2),1)
+        
+        return self.conv(x)
+    
+    
+class Unet_model(nn.Module):
+    def __init__(
+        self):
+        super().__init__()
+        # enocder
+        self.down_1 = DownSample(
+                input_channel=1,
+                out_channel=4, 
+                kernel_size=(3, 3), 
+                stride=(2, 1),
+                activation = nn.GELU(),
+                normalization = nn.BatchNorm2d(4)
+            )
+        self.down_2 = DownSample(
+                input_channel=4,
+                out_channel=8, 
+                kernel_size=(3, 3), 
+                stride=(2, 1),
+                activation = nn.GELU(),
+                normalization = nn.BatchNorm2d(8)
+            )
+        self.down_3 = DownSample(
+                input_channel=8,
+                out_channel=16, 
+                kernel_size=(3, 3), 
+                stride=(2, 1),
+                activation = nn.GELU(),
+                normalization = nn.BatchNorm2d(16)
+            )
+        self.down_4 = DownSample(
+            input_channel=16, 
+            out_channel=32, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(32)
+        )
+        
+        self.down_5 = DownSample(
+            input_channel=32, 
+            out_channel=64, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(64)
+        )
+        self.down_6 = DownSample(
+            input_channel=64, 
+            out_channel=128, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(128)
+        )
+        # bottleneck
+        self.lstm = nn.LSTM(input_size=128*2, 
+                            hidden_size=128*2, 
+                            num_layers=1, 
+                            batch_first=True)
+        # decoder
+        self.up_0 = UpSample(
+            input_channel=128*2, 
+            out_channel=64, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(64)
+        )
+        
+        self.up_1 = UpSample(
+            input_channel=64*2, 
+            out_channel=32, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(32)
+        )
+        self.up_2 = UpSample(
+            input_channel=32*2, 
+            out_channel=16, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(16)
+        )
+        self.up_3 = UpSample(
+            input_channel=16*2, 
+            out_channel=8, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(8)
+        )
+        self.up_4 = UpSample(
+            input_channel=8*2, 
+            out_channel=4, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(4)
+        )
+        self.up_5 = UpSample(
+            input_channel=4*2, 
+            out_channel=2, 
+            kernel_size=(3,3), 
+            stride=(2,1), 
+            activation=nn.GELU(), 
+            normalization=nn.BatchNorm2d(2)
+        )
+        self.convT = CausalConvTranspose2d(
+            in_channels=2, 
+            out_channels=1,
+            kernel_size=(2,3),
+            stride=(1,1),
+        )
+    def forward(self, x):
+        B, C, F, T = x.shape # [B, 1, 200, T]
+        
+        x1 = self.down_1(x) # -> [B, 4, 99, T]
+        x2 = self.down_2(x1) # -> [B, 8, 49, T]
+        x3 = self.down_3(x2) # -> [B, 16, 24, T]
+        x4 = self.down_4(x3) # -> [B, 32, 11, T]
+        x5 = self.down_5(x4) # -> [B, 64, 5, T]
+        x6 = self.down_6(x5) # -> [3, 128, 2, 400]
+        
+        _, C_c, F_c, _ = x6.shape
+        x = x6.view(B, -1, T).permute(0, 2, 1)  # (B, C, F, T) -> (B, C*F, T) -> (B, T, C*F): [B, T, 640]
+        x = self.lstm(x)[0]
+        x = x.permute(0, 2, 1).view(B, C_c, F_c, T) # -> [B, 128, 5, T]
+
+        x = self.up_0(x, x6) # -> [B, 64, 5, T]
+        x = self.up_1(x, x5) # -> [B, 32, 11, T]
+        x = self.up_2(x, x4) # -> [B, 16, 23, T]
+        x = self.up_3(x, x3) # -> [B, 8, 49, T]
+        x = self.up_4(x, x2) # -> [B, 4, 99, T]
+        x = self.up_5(x, x1) # -> [B, 2, 199, T]
+        return self.convT(x) # -> [B, 1, 200, T]
